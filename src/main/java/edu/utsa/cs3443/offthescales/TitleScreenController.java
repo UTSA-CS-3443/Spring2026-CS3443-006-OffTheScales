8f6896ab5c1baae5ba265b839cd6397d868d682a
ABryant46 <aiden.bryant2017@gmail.com>
2026-04-02 23:58:07 -0500

    created basic title screen and it's controller

--- src/main/java/edu/utsa/cs3443/offthescales/TitleScreenController.java
+++ src/main/java/edu/utsa/cs3443/offthescales/TitleScreenController.java
@@ -1,4 +1,25 @@
 package edu.utsa.cs3443.offthescales;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class TitleScreenController {
+
+    @FXML
+    private Button LeaderboardButton;
+
+    @FXML
+    private Button SongSelectButton;
+
+    @FXML
+    void LeaderboardClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void SongSelectClicked(MouseEvent event) {
+
+    }
+
 }
