9f84f27faa21a5c4a3277527c4851a11b757b753
ABryant46 <aiden.bryant2017@gmail.com>
2026-04-03 01:05:59 -0500

    update driver

--- src/main/java/edu/utsa/cs3443/offthescales/Driver.java
+++ src/main/java/edu/utsa/cs3443/offthescales/Driver.java
@@ -9,10 +9,6 @@ import java.io.IOException;
 
 public class Driver extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Driver.class.getResource("TitleScreen.fxml"));
